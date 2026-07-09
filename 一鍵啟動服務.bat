cc6b97b925ebf35a9bba4468b39032b6354f4252
Typelin <typelin@github.com>
2026-07-10 00:14:45 +0800

    移除批次檔中的中文字，改用純 ASCII 解決 Windows 終端機預先載入解譯錯誤

--- 一鍵啟動服務.bat
+++ 一鍵啟動服務.bat
@@ -1,7 +1,6 @@
-REM UTF-8
 @echo off
 chcp 65001 >nul
-title 一鍵啟動測試服務
-echo 正在一鍵啟動測試服務與背景穿牆隧道...
+title Launching Server
+echo Starting Server and Cloudflare Tunnel...
 cd local_db_api
 python server.py
